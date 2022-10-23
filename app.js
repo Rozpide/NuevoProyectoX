7b2890e9f8538431dbf95a5e4c4b9a2ad00b59ec
Rozpide <joserozpide@yahoo.es>
2022-10-23 14:26:35 +0200

    cambio-231022-2

---
+++ app.js
@@ -0,0 +1,9 @@
+function app() {
+  var x = 0;
+  var y = 0;
+  if (x==0,x++) {
+    y++;
+  }
+
+}
+console.log(app());
--- proyectoX.js
+++
@@ -1,8 +0,0 @@
-function proyectoX(){
-  console.log('proyectoX');
-}
-if (typeof exports !== 'undefined') {
-  exports.proyectoX = proyectoX;
-}
-
-    
